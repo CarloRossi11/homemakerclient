5907395bed8707f9ef7d62daa245f6ea4283743c
Carlo Berardelli <owner@Carlos-MBP.fios-router.home>
2020-12-08 21:11:58 -0500

    connected frontend to backend API

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
+
+
 const ProjectDisplay = () => {
+
+    const getProjects = async () => {
+        const response = await fetch("http://localhost:3000/projects")
+        const data = await response.json()
+        console.log(data)
+    }
+
+    React.useEffect(() => {
+        getProjects()
+    }, [])
   
     return (
         <>
