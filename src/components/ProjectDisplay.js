d85fb76b83152101411cc0a935924ba941d4b1ae
Carlo Berardelli <owner@Carlos-MBP.fios-router.home>
2020-12-08 20:06:14 -0500

    set up front end and ProjectDisplay component

---
+++ src/components/ProjectDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectDisplay = () => {
+  
+    return (
+        <>
+          <h1>Projects Displayed Here</h1>
+        </>
+    )
+}
+
+export default ProjectDisplay
