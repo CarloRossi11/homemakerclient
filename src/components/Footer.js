c387efd9b2c4c0a00962f3dd486cfe72d70e844f
Carlo Berardelli <carloberardelli@yahoo.com>
2021-01-22 22:18:19 -0500

    added link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(<footer className="smalls">
-        <p>Built by Carlo Berardelli</p>
+        <a className="port" href="https:carloberardelli.com" target="_blank">Carlo Berardelli 2020</a>
     </footer>)
 }
 
